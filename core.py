9f069c056b9328aa285f4ab5ebcfc8ce5a9cc962
Martin Spacek <git@mspacek.mm.st>
2006-04-26 03:35:45 -0700

    starting to define classes

---
+++ core.py
@@ -0,0 +1,33 @@
+'''Core neuropy functions and classes'''
+
+
+class Cat:
+	'''A Cat can have multiple Tracks and Experiments'''
+	def __init__():
+
+	def load():
+
+class Track:
+	'''A Track can have multiple Experiments'''
+	def __init__():
+
+class Experiment:
+	'''An Experiment can have multiple Neurons and Stims'''
+	def __init__(folder='path/to/Experiment'):
+		self.n = []; # neuron list
+		self.s = []; # stim list
+
+	def load():
+		for neuron in neurons:
+			n.append(neuron)
+
+		for stim in stims:
+			s.append(stim)
+
+class Neuron:
+	def __init__():
+
+	def load():
+
+class Stim:
+	def __init__():
