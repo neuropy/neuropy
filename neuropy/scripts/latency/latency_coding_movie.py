26b319075be48f0b6938e0c148519331b2dfe3cd
Martin Spacek <git@mspacek.mm.st>
2008-04-16 15:17:12 -0700

    add latency coding movie generation script

---
+++ neuropy/scripts/latency/latency_coding_movie.py
@@ -0,0 +1,58 @@
+"""Creates a latency coding movie from an image, to try and change perception
+of the image by manipulating its latency-contrast profile"""
+
+from __future__ import division
+
+import Image
+import numpy as np
+import sys
+import os
+import struct
+
+nframes = 4
+moviepath = '/mov/latency/'
+
+assert len(sys.argv) <= 2
+if len(sys.argv) == 2:
+    fname = sys.argv[1]
+else:
+    fname = 'lincoln_head_small.jpg'
+print 'Processing image file', fname
+
+im = Image.open(fname)
+# could just convert to luminance, but warn instead for now
+assert im.mode == 'L', 'image file should be luminance only (greyscale)'
+imdata = np.asarray(im)
+height, width = imdata.shape
+meanval = int(round(imdata.mean()))
+print 'Mean pixel value is', meanval
+maxval = imdata.max()
+minval = imdata.min()
+span = maxval - minval
+step = span / nframes
+threshes = [minval + (i+1)*step for i in range(nframes)] # pixel threshold values
+
+# init movie
+frames = meanval * np.ones((nframes, height, width), dtype=np.uint8) # all pixels same as mean image pix val
+
+for framei, thresh in enumerate(threshes):
+    if framei == 0:
+        condition = (imdata <= thresh)
+    else:
+        condition = (threshes[framei-1] < imdata) * (imdata <= thresh)
+    xy = condition.nonzero() # tuple of x and y indices where condition is true
+    frames[framei][xy] = imdata[xy]
+
+# save to a movie file
+outfname, ext = os.path.splitext(fname)
+outfname = outfname.upper() # same as image name, no ext, all caps
+print 'Writing to movie file', moviepath+outfname
+f = file(moviepath+outfname, 'wb') # open a movie file for writing in binary format
+# write movie header
+f.write('movie')
+f.write(struct.pack('H', width)) # 'H'== unsigned short int == 2 bytes on this PC
+f.write(struct.pack('H', height))
+f.write(struct.pack('H', nframes))
+for frame in frames:
+    f.write(frame)
+f.close()
