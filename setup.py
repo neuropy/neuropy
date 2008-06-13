237d03187c9f5e5b83b78c1dece53bd8fb7bff41
Martin Spacek <git@mspacek.mm.st>
2008-06-13 14:43:56 -0700

    this rev posted on website

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
-"""neuropy installation script"""
+"""neuropy installation script
+
+to create source distribution and force tar.gz file:
+>>> python setup.py sdist --formats=gztar
+to create binary distribution:
+>>> python setup.py bdist_wininst
+"""
 
 from distutils.core import setup
 import os
 
 setup(name='neuropy',
-      #version='',
-      description='Neuronal data analysis in Python',
+      version='0.1',
+      license='BSD',
+      description='Neuronal spike data and stimulus analysis in Python',
       author='Martin Spacek',
-      #author_email='',
-      #url='',
+      author_email='mspacek at interchange ubc ca',
+      url='http://swindale.ecc.ubc.ca/neuropy',
       #long_description='',
-      packages=['neuropy', 'neuropy.scripts'])
+      #packages=['neuropy', 'neuropy.scripts'])
+      packages=['neuropy'])
