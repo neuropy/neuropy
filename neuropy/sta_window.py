113ebdd1274b3433285fff5be3ebe3a1b9476f89
Martin Spacek <git@mspacek.mm.st>
2011-06-04 02:01:41 -0700

    clean up STAWindow

--- neuropy/sta_window.py
+++ neuropy/sta_window.py
@@ -1,12 +1,17 @@
-"""STA window in Qt"""
+"""STA window in Qt. I think what's necessary to make this work in
+ipython-qtconsole is to have neuropy (via ipython kernel somehow) send
+a zmq message (display_data) back to the frontend telling it to create
+an STA window, as well as the results of the STA calc in the message payload
+or something. mpl must be doing this every time you make a plot, 
+but have to verify"""
 
 from __future__ import division
 
 import sys
 
 from PyQt4 import QtCore, QtGui
-from PyQt4.QtGui import QPixmap, QImage
-from PyQt4.QtCore import QSize
+from PyQt4.QtGui import QPixmap, QImage, QPalette, QColor
+from PyQt4.QtCore import Qt, QSize
 
 import numpy as np
 import scipy as sp
@@ -20,55 +25,44 @@ class STAWindow(QtGui.QMainWindow):
         QtGui.QMainWindow.__init__(self)
         ts = np.arange(0, 200, 20)
         nids = np.arange(100)
-        nt = len(ts)
-        nnids = len(nids)
-        pixwidth, pixheight = 32, 32
-        scale = 2
+        width, height = 32, 32
+        scale = 2 # setting to a float will give uneven sized pixels
 
         cmap = mpl.cm.jet(np.arange(256), bytes=True) # 8 bit RGBA colormap
         #cmap[:, [0, 1, 2, 3]] = cmap[:, [3, 0, 1, 2]] # 8 bit ARGB colormap
-        colortable = cmap.view(dtype=np.uint32).flatten().tolist()# const QVector<QRgb> colors 
+        # from Qt docs, sounds like I should be using ARGB format, but seems like
+        # RGBA is the format that works in PyQt4
+        colortable = cmap.view(dtype=np.uint32).ravel().tolist() # QVector<QRgb> colors 
+        layout = QtGui.QGridLayout() # can set vert and horiz spacing
+        #layout.setContentsMargins(0, 0, 0, 0) # doesn't seem to do anything
 
-        #print(QtGui.QColor(255, 255, 255, 0).rgba())
-
-        layout = QtGui.QGridLayout()
-        #layout.setContentsMargins(0, 0, 0, 0)
-        for ti, t in enumerate(ts):
+        for ti, t in enumerate(ts): # time labels along top
             label = QtGui.QLabel(str(t))
             layout.addWidget(label, 0, ti+1)
         for ni, nid in enumerate(nids):
-            label = QtGui.QLabel('n'+str(nid))
+            label = QtGui.QLabel('n'+str(nid)) # nid label on left
+            label.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
             layout.addWidget(label, ni+1, 0)
             for ti, t in enumerate(ts):
-                #data = np.uint8(np.random.randint(0, 255, size=(pixheight, pixwidth, 4))) # RGBA
-                data = np.uint8(np.random.randint(0, 255, size=(pixheight, pixwidth)))
-                image = QImage(data.data, pixwidth, pixheight, QImage.Format_Indexed8)
+                data = np.uint8(np.random.randint(0, 255, size=(height, width)))
+                image = QImage(data.data, width, height, QImage.Format_Indexed8)
                 image.ndarray = data # hold a ref, prevent gc
                 image.setColorTable(colortable)
-                #print(image.colorTable())
-                #for ci in range(256):
-                #    argb = int(cmap[ci].view(dtype=np.uint32))
-                #    image.setColor(ci, argb)
-                pixmap = QPixmap.fromImage(image.scaled(QSize(scale*pixwidth, scale*pixheight)))
-                
-                #pixmap = QPixmap(32*scale, 32*scale)
-                #data = np.uint8(np.random.randint(0, 255, size=(pixheight, pixwidth, 4))) # RGBA
-                #data = sp.ndimage.zoom(data, scale, order=0)
-                #pixmap.loadFromData(data.data, 'uint')
-                #pixmap.ndarray = data # don't let numpy array get gc'd
-                # can also call image.setColorTable or setColor, and then use Format_Indexed8 format into it
+                image = image.scaled(QSize(scale*width, scale*height)) # scale it
+                pixmap = QPixmap.fromImage(image)
                 label = QtGui.QLabel()
                 label.setPixmap(pixmap)
                 layout.addWidget(label, ni+1, ti+1) # can also control alignment
 
-
         mainwidget = QtGui.QWidget(self)
-        mainwidget.setLayout(layout) # or addlayout?
+        mainwidget.setLayout(layout)
 
         scrollarea = QtGui.QScrollArea()
-        scrollarea.setWidget(mainwidget) # or maybe instead of using setwidget, you just call setlayout or addlayout?
-        
+        scrollarea.setWidget(mainwidget)
+
         self.setCentralWidget(scrollarea)
+        #palette = QPalette(QColor(255, 255, 255))
+        #self.setPalette(palette) # set white background, or perhaps more
 
 
 if __name__ == "__main__":
