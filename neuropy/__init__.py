23bd9ac7079fc9fd82ca8ca4580fe1c325c719b8
Martin Spacek <git@mspacek.mm.st>
2013-02-22 17:46:59 -0800

    Move object hierarchy from __init__ to README

--- neuropy/__init__.py
+++ neuropy/__init__.py
@@ -1,29 +1,4 @@
-r"""Neuronal data analysis in Python
-
-          object hierarchy:
-
-                                level:
-
-                Animal            1
-                  |
-                Track             2             Track
-                  |                               |
-              Recording           3           TrackSort
-             /         \                          |
-       Experiment      Sort       4          TrackNeuron
-            |           |
-      (Cat15Movie)    Neuron      5
-
-As for variable scope, this is how things work:
-
-Main Qt event loop creates a
-    NeuropyWindow which runs an
-        IPython session which imports
-            neuropy modules
-    
-So, something like get_ipython() only works from within those neuropy modules that have been
-imported during the IPython session, but not in any of NeuropyWindow's methods.
-"""
+r"""Neuronal data analysis in Python"""
 
 __authors__ = ["Martin Spacek"]
 __version__ = '0.3'
