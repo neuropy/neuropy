ddea2ad739bbe87f4fd8f73c9497c75c4a6556b1
Martin Spacek <git@mspacek.mm.st>
2013-03-27 00:20:58 -0700

    Better testing of Cython mean pre-calc

--- neuropy/test_cc_tranges.py
+++ neuropy/test_cc_tranges.py
@@ -7,19 +7,24 @@ import util # .pyx file
 import numpy as np
 import time
 
-#a = np.arange(20, dtype=np.float64)
-#a.shape = 4, 5
-m = 5
-n = 10
-c = np.zeros((m, n), dtype=np.int8)
-c[0,0] = 1
-c[4,7] = 1
-c[4,4] = 1
+nn = 5 # number of neurons
+nt = 100
+c = np.int8(np.random.randint(2, size=(nn, nt)))
+print(c)
 
-t = np.arange(n, dtype=np.int64)
-nids = np.arange(m, dtype=np.int64)
-ntranges = 100
-tranges = np.arange(ntranges*2, dtype=np.int64)
-tranges.shape = ntranges, 2
+nids = np.arange(nn, dtype=np.int64)
+t = np.arange(nt, dtype=np.int64)
+#ntranges = 10
+tranges = np.array([[0, 10], [10,20], [20,30], [30,40], [40,50],
+                    [50,60], [60,70], [70,80], [80,90], [90,100]], dtype=np.int64)
+ntranges = len(tranges)
+tis = t.searchsorted(tranges) # ntranges x 2 array
+means = np.zeros((ntranges, nn))
+for i, (lo, hi) in enumerate(tis):
+    means[i] = c[:, lo:hi].mean(axis=1)
+print('python means:')
+print(means)
+
+#print(c.mean(axis=1))
 
 util.cc_tranges(c, t, nids, tranges)
