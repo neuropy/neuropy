ec4394f2822461d5cb88a7f0be3636e4cfcb1d54
Martin Spacek <git@mspacek.mm.st>
2013-04-04 11:20:11 -0700

    Change default markersize

--- neuropy/startup.py
+++ neuropy/startup.py
@@ -43,3 +43,4 @@ ip.Completer.greedy = True
 ip.Completer.omit__names = 1 # all 'magic' names (``__foo__``) will be excluded
 rc = get_ipython().user_ns['rcParams']
 rc['mathtext.default'] = 'regular' # use same font for math mode as regular text mode
+rc['lines.markersize'] = 5 # default 6 renders yucky diamond-like points
