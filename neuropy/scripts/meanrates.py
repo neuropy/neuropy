708b32baa6397f10470fa41b02cd9d94e48aa1b1
Martin Spacek <git@mspacek.mm.st>
2013-11-17 16:01:55 -0800

    Improve meanrates xlabel, add printouts

--- neuropy/scripts/meanrates.py
+++ neuropy/scripts/meanrates.py
@@ -8,10 +8,14 @@ f = figure()
 for track, c in zip(tracks, colours):
     trackrates = sorted([ n.meanrate for n in track.alln.values() ])[::-1] # reverse order
     plot(trackrates, '-', c=c, linewidth=3, label=track.absname)
+    trackrates = np.asarray(trackrates)
+    n = len(trackrates)
+    print('%.2f %% neurons < 1 Hz' % ((trackrates < 1).sum() / n * 100))
+    print('%.2f %% neurons < %.2f Hz' % ((trackrates < MINRATE).sum() / n * 100, MINRATE))
 
 axhline(y=MINRATE, c='e', ls='--', marker=None)
 legend(frameon=False)
 yscale('log')
-xlabel('neuron count')
+xlabel('neuron rank')
 ylabel('mean firing rate (Hz)')
 f.tight_layout(pad=0.3) # crop figure to contents
