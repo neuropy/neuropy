d182e48cfde97a0e4dfbd69c6887af43804d1e84
Martin Spacek <git@mspacek.mm.st>
2013-12-05 21:28:50 -0800

    Clean up xpospdfs.py

--- neuropy/scripts/xpospdfs.py
+++ neuropy/scripts/xpospdfs.py
@@ -3,22 +3,15 @@ from track.npos"""
 
 figsize = 1.4, 1.4
 
-# for 1a 3 column and 2a 2 column probes
-binw = 14 # um
+# for 1a 3 column and 2a 2 column probes, with 56 um horizontal spacing
+spacing = 56 # um
+binw = spacing / 4 # um
 binw2 = binw / 2
-ledge = -56-binw-binw2
-redge = 56+binw-binw2
+ledge = -spacing-binw-binw2
+redge = spacing+binw-binw2
 edges = np.arange(ledge, redge+2*binw, binw)
 #edges = np.arange(-56-14, 56+2*14+14, 14) - 14/2
 
-'''
-# for 2a 2 column probes:
-binw = 7
-binw2 = binw / 2
-ledge = -28-binw-binw2
-redge = 28+binw-binw2
-edges = np.arange(ledge, redge+2*binw, binw)
-'''
 # 2a 2 column probe:
 ptc15.tr7c.pospdf(dim='x', edges=edges, figsize=figsize, labels=False)
 xticks((-28, 28))
