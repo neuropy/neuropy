eefa19b68fdc08d4bfce96472293b4131a9b1152
Martin Spacek <git@mspacek.mm.st>
2013-12-07 17:56:36 -0800

    Add mean and lognormal model rank order lines to meanrates.py

--- neuropy/scripts/meanrates.py
+++ neuropy/scripts/meanrates.py
@@ -6,11 +6,13 @@ fontsize(18)
 tracks = [ptc15.tr7c, ptc22.tr1, ptc22.tr2]
 colours = 'r', 'g', 'b'
 nbins = 25
-f = figure()
+
+# Figure 1: plot firing rate rank order:
+f1 = figure(1)
 allrates = []
 for track, c in zip(tracks, colours):
     trackrates = sorted([ n.meanrate for n in track.alln.values() ])[::-1] # reverse order
-    plot(trackrates, '-', c=c, linewidth=3, label=track.absname)
+    plot(trackrates, '--', c=c, linewidth=3, label=track.absname)
     trackrates = np.asarray(trackrates)
     n = len(trackrates)
     print(track.absname)
@@ -18,33 +20,37 @@ for track, c in zip(tracks, colours):
     print('%.2f %% neurons < %.2f Hz' % ((trackrates < MINRATE).sum() / n * 100, MINRATE))
     allrates.append(trackrates)
 allrates = np.hstack(allrates)
+allrates.sort()
+allrates = allrates[::-1] # decreasing order
 n = len(allrates)
+
+# plot rank order of all cells across all tracks, in black:
+allx = np.arange(n) / len(tracks) # compress x values to fit, effectively the mean
+plot(allx, allrates, '--', c='k', linewidth=3, label='mean')
+
 print('%.2f %% total neurons < 1 Hz' % ((allrates < 1).sum() / n * 100))
 print('%.2f %% total neurons < %.2f Hz' % ((allrates < MINRATE).sum() / n * 100, MINRATE))
 
-axhline(y=MINRATE, c='e', ls='--', marker=None)
-legend(frameon=False)
-yscale('log')
-xlabel('neuron rank')
-ylabel('mean firing rate (Hz)')
-gcfm().window.setWindowTitle('meanrates_rank')
-f.tight_layout(pad=0.3) # crop figure to contents
-
-f = figure()
+# Figure 2: plot meanrates PDF, distribution as y for modelling
+f2 = figure(2)
 logallrates = np.log10(allrates)
 logmin, logmax = min(logallrates), max(logallrates)
 logstart, logend = np.floor(logmin), np.ceil(logmax)
-edges = np.logspace(logstart, logend, nbins+1)
-# plot meanrates PDF:
+edges = np.logspace(logstart, logend, nbins+1) # nbins+1 points in log space
+
 y = hist(allrates, bins=edges, color='k')[0]
 logx = np.log10(edges)[:-1] # left bin edges
 logx = logx + (logx[1] - logx[0]) / 2# middle of bins
+
 # do least-squares LM of fit lognormal distribution to data:
+
 from core import g
 from scipy.optimize import leastsq
+
 def cost(p, x, y):
     mu, sigma, A = p
     return A * g(mu, sigma, x) - y
+
 logmean = -1
 logsigma = 1
 A = max(y)
@@ -52,10 +58,14 @@ p = logmean, logsigma, A
 result = leastsq(cost, p, args=(logx, y), full_output=True)
 p, cov_p, infodict, mesg, ier = result
 mu, sigma, A = p
-modelx = np.logspace(logstart, logend, 200)
-# plot lognormal model:
-plot(modelx, A*g(mu, sigma, np.log10(modelx)), 'r-', linewidth=3)
-axvline(x=MINRATE, c='e', ls='--', marker=None)
+modelx = np.logspace(logstart, logend, 200) # 200 points in log space
+modely = A*g(mu, sigma, np.log10(modelx))
+
+# Figure 2: plot lognormal model in magenta:
+plot(modelx, modely, 'm-', linewidth=3)
+
+# Figure 2: finish up
+axvline(x=MINRATE, c='e', ls='--', marker=None) # plot threshold
 xscale('log')
 xlabel('mean firing rate (Hz)')
 ylabel('neuron count')
@@ -64,7 +74,23 @@ text(0.98, 0.98, 'log($\mu$) = %.2f\n'
                  horizontalalignment='right',
                  verticalalignment='top',
                  transform=gca().transAxes,
-                 color='red')
-
+                 color='m')
 gcfm().window.setWindowTitle('meanrates_pdf')
-f.tight_layout(pad=0.3) # crop figure to contents
+f2.tight_layout(pad=0.3) # crop figure to contents
+
+# Figure 1: now plot rank order of lognormal distrib of cells across all tracks, in magenta:
+figure(1)
+modelx, modely
+rank = np.cumsum(modely)
+rank = rank / rank.max() * allx.max() # make it range
+rank = rank[::-1] # decreasing order
+plot(rank, modelx, 'm-', linewidth=3, label='lognormal')
+
+# Figure 1: finish up
+axhline(y=MINRATE, c='e', ls='--', marker=None) # plot threshold
+legend(frameon=False)
+yscale('log')
+xlabel('neuron rank')
+ylabel('mean firing rate (Hz)')
+gcfm().window.setWindowTitle('meanrates_rank')
+f1.tight_layout(pad=0.3) # crop figure to contents
