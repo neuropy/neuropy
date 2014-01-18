08eb4fb46645396a29bb51d123a0a04c0dbd3ead
Martin Spacek <git@mspacek.mm.st>
2014-01-17 17:59:32 -0800

    Also plot sigma distribs track-wise, clean up for figure

--- neuropy/scripts/sigma_distrib.py
+++ neuropy/scripts/sigma_distrib.py
@@ -1,10 +1,37 @@
-"""Plot distribution of spatial sigmas of designated tracks"""
+"""Plot distribution of spatial sigmas of designated tracks. Run from within neuropy
+using `run -i scripts/sigma_distrib.py`"""
+
+from __future__ import division
+from __future__ import print_function
 
 tracks = [ptc15.tr7c, ptc22.tr1, ptc22.tr2] # need to be loaded ahead of time
+bw = 5
+bins = np.arange(0, 100+bw, bw)
 
 sigmas = []
 for track in tracks:
-    for n in track.alln.values():
-        sigmas.append(n.sigma)
+    tracksigmas = [ n.sigma for n in track.alln.values() ]
+    sigmas.append(tracksigmas)
+    figure(figsize=(3, 3))
+    hist(tracksigmas, fc='k', bins=bins)
+    xlim(xmax=110)
+    ylim(ymax=20)
+    yticks((0, 10, 20))
+    xticks((0, 25, 50, 75, 100))
+    xlabel('$\sigma$ ($\mu$m)')
+    ylabel('neuron count')
+    gcfm().window.setWindowTitle(track.absname)
+    gcf().tight_layout(pad=0.2) # crop figure to contents
 
-hist(sigmas, bins=20) # bins=40 looks a little more tantalizing
+sigmas = np.hstack(sigmas)
+figure(figsize=(3, 3))
+hist(sigmas, fc='k', bins=bins) # bins=40 looks a little more tantalizing
+xlim(xmax=110)
+ylim(ymax=50)
+xticks((0, 25, 50, 75, 100))
+yticks((0, 25, 50))
+xlabel('$\sigma$ ($\mu$m)')
+ylabel('neuron count')
+gcfm().window.setWindowTitle('all tracks')
+gcf().tight_layout(pad=0.2) # crop figure to contents
+show()
