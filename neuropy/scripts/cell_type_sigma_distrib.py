3ac08f5945c34c1b307042e1b2378857bdf0486d
Martin Spacek <git@mspacek.mm.st>
2014-01-19 20:08:20 -0800

    Use slightly finer time bins for all tracks

--- neuropy/scripts/cell_type_sigma_distrib.py
+++ neuropy/scripts/cell_type_sigma_distrib.py
@@ -7,6 +7,8 @@ from __future__ import print_function
 tracks = [ptc15.tr7c, ptc22.tr1, ptc22.tr2] # need to be loaded ahead of time
 bw = 5
 bins = np.arange(0, 100+bw, bw)
+alltracksbw = 4
+alltracksbins = np.arange(0, 100+alltracksbw, alltracksbw)
 
 sigmas = []
 for track in tracks:
@@ -25,11 +27,11 @@ for track in tracks:
 
 sigmas = np.hstack(sigmas)
 figure(figsize=(3, 3))
-hist(sigmas, fc='k', bins=bins) # bins=40 looks a little more tantalizing
+hist(sigmas, fc='k', bins=alltracksbins)
 xlim(xmax=110)
-ylim(ymax=50)
+#ylim(ymax=40)
 xticks((0, 25, 50, 75, 100))
-yticks((0, 25, 50))
+yticks((0, 20, 40))
 xlabel('$\sigma$ ($\mu$m)')
 ylabel('neuron count')
 gcfm().window.setWindowTitle('all tracks')
