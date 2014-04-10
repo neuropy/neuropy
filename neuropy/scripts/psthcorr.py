031a7461e33b36b0186806ae44d986b8b06258bd
Martin Spacek <git@mspacek.mm.st>
2014-04-10 13:47:11 -0700

    Use strange to split recs in psthcorr, nicer plotting

--- neuropy/scripts/psthcorr.py
+++ neuropy/scripts/psthcorr.py
@@ -8,38 +8,62 @@ common = False # use common set of active nids within a recording list?
 showcolorbar = False # show colorbar
 sepbinw = 200 # separation bin width, um
 
-ptc15tr7crecs = [ptc15.tr7c.r74, ptc15.tr7c.r95b]
-ptc22tr2recs  = [ptc22.tr2.r33, ptc22.tr2.r28] # 28 is a 5 min movie
-#synched = [ptc22.tr1.r08s, ptc22.tr1.r10s]
-#desynched = [ptc22.tr1.r08d, ptc22.tr1.r10d]
+#ptc15tr7crecs = [ptc15.tr7c.r74, ptc15.tr7c.r95b]
 
-def psthcorr(rec, nids=None, natexps=False):
+ptc22tr1recs = [ptc22.tr1.r08, ptc22.tr1.r08, ptc22.tr1.r10, ptc22.tr1.r10]
+stranges = [(0, 1500), # r08 desynched
+            (1550, np.inf), # r08 synched, end is ~ 2300
+            (0, 1400), # r10 synched
+            (1480, np.inf)] # r10 desynched, end is ~ 2300
+
+#ptc22tr2recs  = [ptc22.tr2.r33, ptc22.tr2.r28] # 28 is a 5 min movie
+"""
+## TODO: make rec method that takes strange and returns list of nids that are active within
+that trange. The, do this for every rec section to be plotted here. For the rho matrix, use
+the superset of all nids to be compared, and leave set to -1 for those rec sections for which
+a nid is inactive. This will result in entire rows and columns coloured dark blue, but will
+allow direct comparison of as many cell pairs as possible across rec sections. Would be nice
+to color them black or white instead of dark blue. Hard to do with the cmap? Maybe used masked
+arrays. What would a cmap do with an entry that's masked? Leave it white like the background?
+"""
+
+def psthcorr(rec, nids=None, natexps=False, strange=None):
     if nids == None:
         nids = sorted(rec.n)
+    midbins, psths, nids = rec.traster(nids=nids, natexps=natexps, strange=strange, plot=False,
+                                       psth=True, binw=0.02, tres=0.005, norm=True)
     nn = len(nids)
-    npairs = nCr(nn, 2)
     nidticks = np.arange(0, nn, 10)
-    midbins, psths = rec.traster(nids=nids, natexps=natexps, plot=False, psth=True, binw=0.02,
-                                 tres=0.005, norm=True)
     rho = np.corrcoef(psths) # defaults to bias=1
+    rho[np.diag_indices(nn)] = 0.0 # null the diagonal
 
     # plot rho matrix:
-    #rho[np.diag_indices(nn)] = 0.0 # null the diagonal
     figure(figsize=figsize)
     imshow(rho, vmin=-1, vmax=1, cmap='jet') # cmap='gray' is too bland
     xticks(nidticks)
     yticks(nidticks)
     if showcolorbar:
         colorbar()
-    gcfm().window.setWindowTitle(rec.absname + '_rho_mat')
+    basetitle = rec.absname
+    if strange != None:
+        basetitle += '_strange=%s' % (strange,)
+    gcfm().window.setWindowTitle(basetitle + '_rho_mat')
     tight_layout(pad=0.3)
 
     # plot rho histogram:
     lti = np.tril_indices(nn, -1) # lower triangle (below diagonal) indices
     rhol = rho[lti]
     figure(figsize=figsize)
-    hist(rhol, bins=30, color='k')
-    gcfm().window.setWindowTitle(rec.absname + '_rho_hist')
+    rhobins = np.arange(-0.3, 1+0.0333, 0.0333) # left edges + rightmost edge
+    n = hist(rhol, bins=rhobins, color='k')[0]
+    axvline(x=rhol.mean(), c='r', ls='--') # draw vertical line at mean rho
+    xlim(xmin=-0.3, xmax=1)
+    ylim(ymax=n.max()) # effectively normalizes the histogram
+    rhoticks = np.arange(-0.2, 1, 0.2) # excluding the final 1
+    xticks(rhoticks)
+    yticks([]) # turn off y ticks for space
+    #yticks([0, n.max()])
+    gcfm().window.setWindowTitle(basetitle + '_rho_hist')
     tight_layout(pad=0.3)
 
     # plot rho vs separation:
@@ -57,23 +81,33 @@ def psthcorr(rec, nids=None, natexps=False):
     rhos = rhol[sortis]
     sepbins = np.arange(0, seps.max()+sepbinw, sepbinw) # left edges
     sepis = seps.searchsorted(sepbins)
-    sepmeans = []
-    rhomeans = []
+    sepmeans, rhomeans, rhosems = [], [], []
     for sepi0, sepi1 in zip(sepis[:-1], sepis[1:]): # iterate over sepbins
-        sepmeans.append(seps[sepi0:sepi1].mean()) # average sep of all points in this bin
-        rhomeans.append(rhos[sepi0:sepi1].mean()) # average rho of all points in this bin
-    plot(sepmeans, rhomeans, 'r.-')
-    septicks = np.arange(0, seps.max()+500, 500)
+        sepmeans.append(seps[sepi0:sepi1].mean()) # mean sep of all points in this sepbin
+        rhoslice = rhos[sepi0:sepi1] # rhos in this sepbin
+        rhomeans.append(rhoslice.mean()) # mean rho of all points in this sepbin
+        rhosems.append(rhoslice.std() / np.sqrt(len(rhoslice))) # SEM of rho in this sepbin
+    #plot(sepmeans, rhomeans, 'r.-', ms=10, lw=2)
+    errorbar(sepmeans, rhomeans, yerr=rhosems, fmt='r.-', ms=10, lw=2)
+    ylim(ymin=-0.3, ymax=1)
+    septicks = np.arange(0, seps.max()+100, 500)
     xticks(septicks)
-    gcfm().window.setWindowTitle(rec.absname + '_rho_sep')
+    yticks(rhoticks)
+    gcfm().window.setWindowTitle(basetitle + '_rho_sep')
     tight_layout(pad=0.3)
 
-
+'''
 nids = None
 if common:
     nids = ptc15.tr7c.get_nids(['74', '95b']) # sorted
 for rec in ptc15tr7crecs:
     psthcorr(rec, nids=nids, natexps=True)
+'''
+nids = None
+if common:
+    nids = ptc22.tr1.get_nids(['08', '10']) # sorted
+for rec, strange in zip(ptc22tr1recs, stranges):
+    psthcorr(rec, nids=nids, natexps=False, strange=strange)
 
 
 show()
