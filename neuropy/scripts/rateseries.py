6a528b0df349c49c23475836d428c2cb5b32a735
Martin Spacek <git@mspacek.mm.st>
2014-06-01 23:43:09 -0700

    Exclude recording time gaps from rate calcs

--- neuropy/scripts/rateseries.py
+++ neuropy/scripts/rateseries.py
@@ -21,39 +21,47 @@ bg = 'k'
 lw = 0.5
 trtrange = [0, 5] # track trange to plot, in hours. Set to None to plot entire tracks
 
-rates = {}
+rates = {} # one entry per track
 for track in tracks:
     trackrates = []
-    # filter out track tranges < width wide (like short CSD recordings):
-    tranges = []
+    # to prevent AssertionError in split_tranges(), filter out track tranges < width wide,
+    # also to prevent unnecessary plotting, exclude track tranges that fall completely
+    # outside trtrange:
+    rectranges = []
     for trange in track.tranges:
-        if trange[1] - trange[0] >= width:
-            tranges.append(trange)
-    tranges = np.array(tranges)
-    # split tranges, so that gaps between recordings are excluded from rate calcs
-    tranges = split_tranges(tranges, width, tres) # possibly overlapping, in us
-    midtranges = tranges.mean(axis=1) / 1e6 / 3600 # midpoints of tranges, in hours
+        if trange[1] - trange[0] >= width: # trange is sufficiently wide
+            if trtrange and trange[0] < trtrange[1]*3600*1e6: # trange starts within trtrange
+                rectranges.append(trange)
+    rectranges = np.array(rectranges)
     nids = np.sort(track.alln.keys())
     figure(figsize=figsize)
     axes(axisbg=bg) # set background color
     for nidi, nid in enumerate(nids):
+        neuronrates = []
         n = track.alln[nid]
-        spikeis = n.spikes.searchsorted(tranges) # slice indices into spikes
-        nspikes = spikeis[:, 1] - spikeis[:, 0] # number of spikes in each trange
-        rate = nspikes / (width / 1e6) # spike rate per bin, in Hz
-        #rate[rate == 0.0] = np.nan # replace 0's with nans so they're ignored by plot()
-        c = CCDICT[nidi] # use nidi to maximize colour alternation
-        plot(midtranges, rate, '-', lw=lw, c=c, alpha=alpha)
-        trackrates.append(rate)
+        # plot rates for each rectrange separately, so lines aren't drawn across time gaps:
+        for rectrange in rectranges: # one per recording
+            tranges = split_tranges([rectrange], width, tres) # possibly overlapping bins, in us
+            midtranges = tranges.mean(axis=1) / 1e6 / 3600 # midpoints of tranges, in hours
+            spikeis = n.spikes.searchsorted(tranges) # slice indices into spikes
+            nspikes = spikeis[:, 1] - spikeis[:, 0] # number of spikes in each trange
+            rate = nspikes / (width / 1e6) # spike rate per bin, in Hz
+            rate[rate == 0.0] = np.nan # replace 0's with nans so they're ignored by plot()
+            c = CCDICT[nidi] # use nidi to maximize colour alternation
+            plot(midtranges, rate, '-', lw=lw, c=c, alpha=alpha)
+            neuronrates.append(rate)
+        trackrates.append(np.hstack(neuronrates))
     yscale('log')
     xlabel('time (h)')
     ylabel('firing rate (Hz)')
-    gcfm().window.setWindowTitle(track.absname)
-    tight_layout(pad=0.3)
-    trackrates = np.vstack(trackrates)
+    trackrates = np.vstack(trackrates) # one row per neuron
     minrate = np.unique(trackrates)[1] # lowest rate, excluding 0 Hz
-    xlim(trtrange)
+    if trtrange:
+        xlim(trtrange)
     ylim(ymin=minrate)
+    titlestr = "%s_width=%d_tres=%d_trange=%s" % (track.absname, widthsec, tressec, trtrange)
+    gcfm().window.setWindowTitle(titlestr)
+    tight_layout(pad=0.3)
     rates[track.absname] = trackrates
 
 show()
