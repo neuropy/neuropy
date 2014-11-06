9f532ee7446860fd8dc6e32e04d9ba724d4c9363
Martin Spacek <git@mspacek.mm.st>
2014-11-05 17:23:23 -0800

    Minor improvements to autocorr_RPVs.py script

--- neuropy/scripts/autocorr_RPVs.py
+++ neuropy/scripts/autocorr_RPVs.py
@@ -4,7 +4,7 @@ neurons. Calculate autocorr of merger, and find its number of RPVs. Take differe
 this and the sum of the individual cell RPVs, normalize by total number of ISIs to get change
 in fraction of refractory period violations df. Sort neuron pairs in decreasing order of df.
 Those with the biggest df have an autocorr that's most affected by the incorrect merger. This
-should help demonstrate that even in the worst case scenario, the autocorr isn't a great poor
+should help demonstrate that even in the worst case scenario, the autocorr isn't a great
 diagnostic of contaminated spike trains, and in the more typical case, it's an absolutely
 terrible diagnostic of contaminated spike trains. Note that in the numerator of the df
 calcalation, I should really be counting number of consecutive ISIs <= RP, not number of spike
@@ -26,15 +26,13 @@ mean firing rate that corresponds to each cell.
 
 import util
 
-RP = 2000 # refractory period, us
+RP = 750 # refractory period, us
 REXCL = 0 # exclusion radius, um
-tracks = ptc15.tr7c, ptc22.tr2, ptc22.tr1 # use this order so ptc22.tr1 comes out on top
-colours = 'lightgrey', 'grey', 'black'
-
-fontsize(16)
-fig = figure(figsize=(7, 4.4))
+tracks = ptc15.tr7c, ptc22.tr1, ptc22.tr2
+colours = 'r', 'g', 'b'
 
 farrs = [] # collect f arrays
+fss = [] # collect fs arrays
 # refractory period violation array dtype, one row per neuron pair,
 # each row: [nid0, nid1, f0, f1, df]
 farrdtype=[('nid0', np.int64), ('nid1', np.int64),
@@ -42,7 +40,7 @@ farrdtype=[('nid0', np.int64), ('nid1', np.int64),
 # convert RP to half trange array, in us
 trange = np.array([0, RP]) # only need to work on one half of the autocorrelogram
 
-for tracki, (track, c) in enumerate(zip(tracks, colours)):
+for track in tracks:
     print(track.absname)
     neurons = track.alln # dict
     nids = sorted(neurons) # sorted keys
@@ -57,6 +55,7 @@ for tracki, (track, c) in enumerate(zip(tracks, colours)):
         nrpvs[nidi] = len(dts)
         nISI = len(s) - 1
         fs[nidi] = nrpvs[nidi] / nISI
+    fss.append(fs)
         
     maxnpairs = nCr(nn, 2) # maximum number of pairs, actual will be less given REXCL
     farr = np.zeros(maxnpairs, dtype=farrdtype)
@@ -87,14 +86,18 @@ for tracki, (track, c) in enumerate(zip(tracks, colours)):
     print()
     print('maxnpairs = %d' % maxnpairs)
     print('actual npairs = %d' % npairs)
+    print('fs descending: %r' % np.sort(fs)[::-1])
     farr = farr[:npairs] # trim down to actual number of pairs
     sortis = np.argsort(farr['df'])
     sortis = sortis[::-1] # reverse
     farr = farr[sortis] # sorted by decreasing df values
     print(farr)
-    plot(farr['df'], ls='-', lw=4, marker=None, c=c, label='track %d' % (tracki+1))
     farrs.append(farr)
 
+fontsize(16)
+fig = figure(figsize=(7, 4.4))
+for track, farr, c in zip(tracks, farrs, colours):
+    plot(farr['df'], ls='-', lw=4, marker=None, c=c, label=track.absname)
 legend(frameon=False, loc='center right')
 xlim(xmin=-20)
 ylim(ymin=-0.0002)
