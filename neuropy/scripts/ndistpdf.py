f362962c72ee592932edbea36c3b6b248fe42c48
Martin Spacek <git@mspacek.mm.st>
2015-02-13 13:24:34 -0800

    Add nearest site dist vs sigma scatter plot to ndistpdf script

--- neuropy/scripts/ndistpdf.py
+++ neuropy/scripts/ndistpdf.py
@@ -4,6 +4,8 @@ specified tracks. Run from within neuropy using `run -i scripts/ndistpdf.py`"""
 from __future__ import division
 from core import dist
 
+MODEL = False
+
 figsize = 3, 3 # inches
 spacing = 65 # um
 binw = 2 # um
@@ -12,7 +14,7 @@ edges = np.arange(0, spacing/2+binw, binw)
 c = 'black'
 tracks = [ptc15.tr7c, ptc22.tr1, ptc22.tr2]
 # ptc15 is 2a, ptc22 tracks are both 1a
-# calculate recordable areas for each polytrode as half a horizontal or vertical spaceing at
+# calculate recordable areas for each polytrode as half a horizontal or vertical spacing at
 # each edge (top, bottom, left right):
 xlims, ylims, areas = [], [], []
 for track in tracks:
@@ -27,14 +29,16 @@ for track in tracks:
     areas.append(w*h) # in square um
 
 N = 0
-ds = []
+ds, sigmas = [], []
 for track in tracks:
     neurons = track.alln.values()
     for neuron in neurons:
         d = [ dist(neuron.pos, cp) for cp in neuron.sort.chanpos ]
         ds.append(min(d))
+        sigmas.append(neuron.sigma)
     N += len(neurons)
 ds = np.hstack(ds)
+sigmas = np.hstack(sigmas)
 
 # calculate theoretical distribution, derived from concentric annuli:
 rho = N / sum(areas) # average cell density per unit polytrode area (1/um^2)
@@ -43,26 +47,27 @@ theory = rho*2*pi*midbins/binw # normed distrib expected for randomly distribute
 
 # simulate numerically by randomly and uniformly distributing points around a polytrode and
 # measuring the distribution of distances between each point and its nearest electrode site:
-umap1a = ptc22.tr1.sort.chanpos
-area1a, xlims1a, ylims1a = areas[1], xlims[1], ylims[1]
-nseed = intround(rho * area)
-nrandom = 100000
-w = xlims1a[1]-xlims1a[0]
-h = ylims1a[1]-ylims1a[0]
-x = np.random.random_sample(nrandom) * w - w/2 # centered around 0
-y = np.random.random_sample(nrandom) * h # all positive
-points = np.vstack([x, y]).T # each row is a point
-modelds = []
-for point in points:
-    d = [ dist(point, cp) for cp in umap1a ]
-    modelds.append(min(d))
-modelds = np.hstack(modelds)
+if MODEL: # can take a long time to run
+    umap1a = ptc22.tr1.sort.chanpos
+    area1a, xlims1a, ylims1a = areas[1], xlims[1], ylims[1]
+    #nseed = intround(rho * area1a)
+    nrandom = 100000
+    w = xlims1a[1]-xlims1a[0]
+    h = ylims1a[1]-ylims1a[0]
+    x = np.random.random_sample(nrandom) * w - w/2 # centered around 0
+    y = np.random.random_sample(nrandom) * h # all positive
+    points = np.vstack([x, y]).T # each row is a point
+    modelds = []
+    for point in points:
+        d = [ dist(point, cp) for cp in umap1a ]
+        modelds.append(min(d))
+    modelds = np.hstack(modelds)
 
 figure(figsize=figsize)
-# underplot hlines at electrode site y positions:
 hist(ds, bins=edges, normed=True, color=c, ec=c)
-model = np.histogram(modelds, bins=edges, density=True)[0]
-plot(midbins, model, 'r--', lw=3)
+if MODEL:
+    model = np.histogram(modelds, bins=edges, density=True)[0]
+    plot(midbins, model, 'r--', lw=3)
 plot(midbins, theory, 'b--', lw=3)
 xlabel('nearest site distance ($\mu$m)')
 ylabel('probability density (1/$\mu$m)')
@@ -70,13 +75,12 @@ xticks((0, gca().get_xticks().max())) # get rid of intermediate xicks
 tight_layout(0.3)
 gcfm().set_window_title("ndistpdf")
 
-#figure(figsize=figsize)
-# underplot hlines at electrode site y positions:
-#plot(edges, theory, 'e--', lw=3)
-#xlabel('nearest site distance ($\mu$m)')
-#ylabel('probability density (1/$\mu$m)')
-#xticks((0, gca().get_xticks().max())) # get rid of intermediate xicks
-#tight_layout(0.3)
-#gcfm().set_window_title("ndistpdf_model")
+figure(figsize=figsize)
+plot(sigmas, ds, 'k.')
+xlabel('template spatial extent ($\mu$m)')
+ylabel('nearest site distance ($\mu$m)')
+xticks(np.arange(25, 100+25, 25))
+tight_layout(0.3)
+gcfm().set_window_title("nearest site distance vs spatial extent")
 
 show()
