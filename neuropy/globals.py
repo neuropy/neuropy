9241375484d1c778032e36d0da4067d3d324dc98
Martin Spacek <git@mspacek.mm.st>
2016-04-25 14:16:10 +0200

    Fix docs typo

--- neuropy/globals.py
+++ neuropy/globals.py
@@ -22,7 +22,7 @@ LOADALLSORTS = False
 
 """Mean spike rate that delineates normal vs "quiet" neurons. 0.1 Hz seems reasonable if you
 plot mean spike rate distributions for all the neurons in a given track. But, if you want a
-reasonable looking DJS histogram withouth a lot of missing netstates, you need to exclude
+reasonable looking DJS histogram without a lot of missing netstates, you need to exclude
 more low firing rate cells, 0.5 works better"""
 MINRATE = 0.05 # Hz
 """Calculate a TrackNeuron's meanrate according to its trange (period between its first and
