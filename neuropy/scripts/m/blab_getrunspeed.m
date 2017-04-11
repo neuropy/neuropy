26bf34f24ad0b299af84d36ce76e294127529ee4
Martin Spacek <git@mspacek.mm.st>
2017-04-11 10:31:18 +0200

    Add Matlab scripts used to generate .mat files

---
+++ neuropy/scripts/m/blab_getrunspeed.m
@@ -0,0 +1,9 @@
+bm = BallMovement('/Volumes/tudata/ephys/Ntsr1-Cre_0174/174-2-005.nev')
+
+roll  = 100*bm.roll(:)';
+pitch = 100*bm.pitch(:)';
+yaw = 100*bm.yaw(:)';
+
+tspeed = bm.timestamps;
+speed = sqrt(roll .^ 2 + pitch .^ 2 + yaw .^ 2);
+save('Ntsr1-Cre_0174_s02_e05_runspeed', 'tspeed', 'speed');
---
+++ neuropy/scripts/m/blab_getstimulustimes.m
@@ -0,0 +1,2 @@
+[stimONTimes, stimOFFTimes, stimSource, nsxFile, p] = getStimulusTimes('Ntsr1-Cre_0174', 2, 5)
+save('Ntsr1-Cre_0174_s02_e05_stim', 'stimONTimes', 'stimOFFTimes', 'stimSource', 'nsxFile', 'p')
