c2bfcd0c497d5e242dd6ced6508f894f3e38ff0b
Martin Spacek <git@mspacek.mm.st>
2017-04-13 13:36:17 +0200

    Handle missing _runspeed.mat file

--- neuropy/scripts/blab_natstate.py
+++ neuropy/scripts/blab_natstate.py
@@ -124,44 +124,45 @@ for trialis, umoviename in zip(trialiss, umovienames):
         nrasterplots += 1
 print('created %d raster plots' % nrasterplots)
 
-# load runspeed info:
-rsd = loadmat(rsfullfname, squeeze_me=True) # dict
-tspeed, speed = rsd['tspeed'], rsd['speed'] # sec, cm/s, speed can contain nans
+if os.path.exists(rsfullfname):
+    # load runspeed info:
+    rsd = loadmat(rsfullfname, squeeze_me=True) # dict
+    tspeed, speed = rsd['tspeed'], rsd['speed'] # sec, cm/s, speed can contain nans
 
-# smooth runspeed using overlapping time bins:
-width, tres = 10, 0.5 # s
-minspeed = 1 # cm/s, otherwise considered at rest
-r.lfp.get_data() # make sure it's loaded so we can access t0 and t1
-t0 = r.lfp.t0 / 1e6 # sec, tspeed starts at t=0, lfp starts a few ms later
-t1 = r.lfp.t1 / 1e6
-tranges = core.split_tranges([(t0, t1)], width, tres) # overlapping time bins
-tiranges = tspeed.searchsorted(tranges)
-nbins = len(tiranges)
-tbinspeed = tranges[:, 0]
-binspeed = np.zeros(nbins)
-for bini, (t0i, t1i) in enumerate(tiranges):
-    binspeed[bini] = np.nanmean(speed[t0i:t1i]) # handle nans with nanmean
-show()
+    # smooth runspeed using overlapping time bins:
+    width, tres = 10, 0.5 # s
+    minspeed = 1 # cm/s, otherwise considered at rest
+    r.lfp.get_data() # make sure it's loaded so we can access t0 and t1
+    t0 = r.lfp.t0 / 1e6 # sec, tspeed starts at t=0, lfp starts a few ms later
+    t1 = r.lfp.t1 / 1e6
+    tranges = core.split_tranges([(t0, t1)], width, tres) # overlapping time bins
+    tiranges = tspeed.searchsorted(tranges)
+    nbins = len(tiranges)
+    tbinspeed = tranges[:, 0]
+    binspeed = np.zeros(nbins)
+    for bini, (t0i, t1i) in enumerate(tiranges):
+        binspeed[bini] = np.nanmean(speed[t0i:t1i]) # handle nans with nanmean
+    show()
 
-# plot runspeed as a color map:
-#figure()
-#plot(tbinspeed, binspeed, '-')
-binspeed.shape = -1, 1 # column vector
-if minspeed:
-    # turn it into a binary rest/run signal:
-    binspeed[binspeed < minspeed] = 0 # at rest
-    binspeed[binspeed >= minspeed] = 1 # running
-f = figure(figsize=(1, 10))
-axis('off')
-plt.imshow(binspeed, aspect=0.025, cmap='gray_r') # white=rest, black=run
-#plt.imshow(binspeed, aspect=0.025, cmap='jet') # blue=rest, red=run
-titlestr = ename + '_runspeed'
-gcfm().window.setWindowTitle(titlestr)
-f.tight_layout(pad=0.3) # crop figure to contents
-show()
+    # plot runspeed as a color map:
+    #figure()
+    #plot(tbinspeed, binspeed, '-')
+    binspeed.shape = -1, 1 # column vector
+    if minspeed:
+        # turn it into a binary rest/run signal:
+        binspeed[binspeed < minspeed] = 0 # at rest
+        binspeed[binspeed >= minspeed] = 1 # running
+    f = figure(figsize=(1, 10))
+    axis('off')
+    plt.imshow(binspeed, aspect=0.025, cmap='gray_r') # white=rest, black=run
+    #plt.imshow(binspeed, aspect=0.025, cmap='jet') # blue=rest, red=run
+    titlestr = ename + '_runspeed'
+    gcfm().window.setWindowTitle(titlestr)
+    f.tight_layout(pad=0.3) # crop figure to contents
+    show()
 
 # plot LFP specgram:
-chani = 31 # 0-based chan ID
+chani = -2 # 0-based chan ID
 r.lfp.specgram(f1=59, chanis=chani, width=2, tres=0.5, cm='jet', relative2t0=True, title=False,
                reclabel=False, figsize=None)
 titlestr = ename + '_specgram_c%d' % (chani+1) # 1-based chan ID
