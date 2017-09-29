efdd5e7e45732e7ba29a7a02cb4b71f67aa88139
Martin Spacek <git@mspacek.mm.st>
2017-09-29 11:09:03 +0200

    Update blab_getstimulustimes.m

--- neuropy/scripts/m/blab_getstimulustimes.m
+++ neuropy/scripts/m/blab_getstimulustimes.m
@@ -1,2 +1,2 @@
-[stimONTimes, stimOFFTimes, stimSource, nsxFile, p] = getStimulusTimes('Ntsr1-Cre_0174', 2, 5)
-save('Ntsr1-Cre_0174_s02_e05_stim', 'stimONTimes', 'stimOFFTimes', 'stimSource', 'nsxFile', 'p')
+[stimONTimes, stimOFFTimes, stimSource, nsxFile, p] = getStimulusTimes('/Volumes/mudata/exp/PVCre_2017_0006/s03/PVCre_2017_0006_s03_e03.ns3');
+save('PVCre_2017_0006_s03_e03_stim', 'stimONTimes', 'stimOFFTimes', 'stimSource', 'nsxFile', 'p');
